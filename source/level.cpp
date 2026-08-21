2097d751e3c3d31046fcf2722e78c5c8caf72f59
alexceend <alexceend@gmail.com>
2026-08-21 13:28:38 +0200

    Prepare Load level from txt logic #1

--- source/level.cpp
+++ source/level.cpp
@@ -5,11 +5,15 @@
 #include "texture.h"
 #include "scene.h"
 #include <list>
+#include <fstream>
+#include <string> 
+#include <sstream>
 
 #define SCREEN_WIDTH 400
 #define SCREEN_HEIGHT 240
 #define GRID_COLS (SCREEN_WIDTH / TILE_SIZE)
 #define GRID_ROWS (SCREEN_HEIGHT / TILE_SIZE)
+
 
 #define EMPTY_TILE -1
 
@@ -19,10 +23,55 @@ static C2D_SpriteSheet currentAtlas;
 static C2D_SpriteSheet atlas_purple_wizard;
 static C2D_SpriteSheet atlas_yellow_wizard;
 
+typedef enum {
+    TILE_EMPTY = -1,
+    TILE_FLOOR_SLAB = 0,
+    TILE_WALL = 15,
+} TileType;
 
-bool testInit(C3D_RenderTarget* target)
+typedef struct {
+    int spawnX;
+    int spawnY;
+} Entity;
+
+typedef struct {
+    uint8_t tiles[16][28];
+    Entity entities[2];
+} Level;
+
+Wizard purpleWizard = {PURPLE, 12.0f, 15.0f, 3, NULL, 0};
+Wizard yellowWizard = {YELLOW, 12.0f, 15.0f, 3, NULL, 0};
+
+bool levelInit(C3D_RenderTarget* target, int levelIndex)
 {
-    
+    audioExit();
+    top = target;
+
+    atlas_dungeon =
+        C2D_SpriteSheetLoad("romfs:/gfx/atlas.t3x");
+
+    atlas_wizard =
+        C2D_SpriteSheetLoad("romfs:/gfx/wizard_atlas.t3x");
+
+    Level level;
+    loadLevelFromFile("romfs:/levels/level1.txt", level);
+
+    // Set spawn points for entities
+    while (getline(file, line)) {
+        if (line.find("spawnPurple") != string::npos) {
+            int x, y;
+            sscanf(line.c_str(), "spawnPurple %d %d", &x, &y);
+            level.entities[0].spawnX = x;
+            level.entities[0].spawnY = y;
+        }else if (line.find("spawnYellow") != string::npos) {
+            int x, y;
+            sscanf(line.c_str(), "spawnYellow %d %d", &x, &y);
+            level.entities[1].spawnX = x;
+            level.entities[1].spawnY = y;
+        }
+    }
+
+    return true;
 }
 
 void loadPhysics()
@@ -32,15 +81,57 @@ void loadPhysics()
     world->SetContactListener(&contactListener);
     world->SetDebugDraw(&fooDrawInstance);
 
-    for (int i = 0; i < SCREEN_WIDTH / TILE_SIZE; i++)
+    for (int i = 0; i < GRID_ROWS; i++)
     {
-        Block* block = new Block;
-        block->row = (SCREEN_HEIGHT / TILE_SIZE) - 2;
-        block->col = i;
-        block->width = 16.0f;
-        block->height = 16.0f;
-        blockList.push_front(block);
-        loadStaticObject(floorImage, block);
+        for (int j = 0; j < GRID_COLS; j++)
+        {
+            if (level.tiles[i][j] != TILE_EMPTY)
+            {
+                Block* block = new Block;
+                block->row = i;
+                block->col = j;
+                block->width = 14.0f;
+                block->height = 14.0f;
+                blockList.push_front(block);
+                loadStaticObject(getAtlasTexture(atlas_dungeon, level.tiles[i][j]), block);
+            }
+        }
     }
-    loadWizardHitbox(150.0f, 0.0f, &w);
+
+    loadWizardHitbox(level.entities[0], 0.0f, purpleWizard);
+    loadWizardHitbox(level.entities[1], 0.0f, yellowWizard);
+}
+
+
+bool loadLevelFromFile(const std::string& filename, Level& level) {
+
+    ifstream file("romfs:/levels/level" + to_string(levelIndex) + ".txt");
+
+    if(!file){
+        printf("ERROR: Could not open level file\n");
+        return false;
+    }
+    
+    while(getline(file, line) && i < 16){
+        std::stringstream ss(line);
+        int tile;
+        int j = 0;
+        while(ss >> tile && j < 28){
+            switch (tile){
+                case -1:
+                    level.tiles[i][j] = TILE_EMPTY;
+                    break;
+                case 0:
+                    level.tiles[i][j] = TILE_FLOOR_SLAB;
+                    break;
+                case 15:
+                    level.tiles[i][j] = TILE_WALL;
+                    break;
+                default:
+                    level.tiles[i][j] = TILE_EMPTY;
+                    break;
+            }
+            j++;
+        }
+    }
 }
