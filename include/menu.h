59e0a13b92229536d4f3baf9a5a5bf1973dffc5e
alexceend <alexceend@gmail.com>
2026-09-22 22:35:03 +0200

    Renamed menu.c to menu.cpp to fix compiling errors

--- include/menu.h
+++ include/menu.h
@@ -5,16 +5,9 @@
 #ifndef MENU_H
 #define MENU_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-    bool menuInit(C3D_RenderTarget* targetTop, C3D_RenderTarget* targetBottom);
-    Scene menuUpdate(u32 kDown);
-    void menuDraw(void);
-    void menuExit(void);
-#ifdef __cplusplus
-}
-#endif
+bool menuInit(C3D_RenderTarget* targetTop, C3D_RenderTarget* targetBottom);
+Scene menuUpdate(u32 kDown);
+void menuDraw(void);
+void menuExit(void);
 
 #endif
